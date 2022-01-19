1c7f7ad796ae613f61716c7a4e122dcfa51d12b9
Yair-Ohana <yairohanap@gmail.com>
2022-01-19 15:00:08 +0200

    useEffect removed, readme updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import { Home } from "./containers/Home";
@@ -7,7 +6,6 @@ import { Master } from "./containers/Master";
 import { Header } from "./components/Header";
 
 function App() {
-  useEffect(() => {}, []);
   return (
     <>
       <Header />
